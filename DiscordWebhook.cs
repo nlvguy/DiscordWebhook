a17f037f2bc9abf48863977a79ace389ea3ee2b0
Nik <nikola.luettgen.vear@gmail.com>
2020-04-21 21:44:56 -0400

    v1.0

---
+++ DiscordWebhook.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DiscordWebhook
+{
+    class DiscordWebhook
+    {
+        private static readonly HttpClient _client = new HttpClient();
+
+        private string _webhookURL;
+        private string _username;
+        private string _avatarUrl;
+
+        public DiscordWebhook(string url) { this._webhookURL = url; }
+        public DiscordWebhook(string url, string username) { this._webhookURL = url; this._username = username; }
+        public DiscordWebhook(string url, string username, string avatarURL) { this._webhookURL = url; this._username = username; this._avatarUrl = avatarURL; }
+
+        public async Task<HttpResponseMessage> SendMessageAsync(string username = null, string avatarUrl = null, string content = null, bool isTTS = false, IEnumerable<Embed> embeds = null)
+        {
+            var msg = new Message(username ?? this._username, avatarUrl ?? this._avatarUrl, content, isTTS, embeds);
+            return await _client.PostAsync(this._webhookURL, new StringContent(JsonConvert.SerializeObject(msg), Encoding.UTF8, "application/json"));
+        }
+
+        [JsonObject]
+        internal class Message
+        {
+            [JsonProperty("username")]
+            public string Username;
+            [JsonProperty("avatar_url")]
+            public string AvatarUrl;
+            [JsonProperty("content")]
+            public string Content;
+            [JsonProperty("tts")]
+            public bool isTTS;
+            [JsonProperty("embeds")]
+            public List<Embed> Embeds;
+
+            public Message(string username, string avatarUrl, string content, bool isTTS, IEnumerable<Embed> embeds)
+            {
+                this.Username = username;
+                this.AvatarUrl = avatarUrl;
+                this.Content = content;
+                this.isTTS = isTTS;
+                Embeds = new List<Embed>(embeds);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# DiscordWebhook
- Discord webhook integration with C# for easy-use
+# Foobar
+
+DiscordWebhook helps easily integrates in any C# application. It let's you send updates or messages to any discord server (provided you have a webhook url).
+
+
+## Dependencies
+[Json.NET](https://github.com/JamesNK/Newtonsoft.Json)
+
+## Customization
+Upon creating the Webhook, you are able to set a default username and avatar that will be used unless specified otherwise when sending a message.
+
+## Example Usage
+
+```cs
+var discord = new DiscordWebhook("Link goes here");
+var testEmbed = new EmbedBuilder()
+    .WithTitle("This is a test embed")
+    .AddField("Field 1", "This is some useful text in field 1", false)
+    .AddField("Field 2 but inline is enabled", "Hi", true)
+    .AddField("Field 3 but it is inline", "im on the right", true)
+    .WithAuthor("nikolalv", "https://github.com/nikolalv", "https://avatars3.githubusercontent.com/u/1341206")
+    .WithColor(250,0,0)
+    .WithTimestamp(DateTime.Now)
+    .WithImage("https://avatars3.githubusercontent.com/u/1341206")
+    .WithThumbnail("https://pbs.twimg.com/profile_images/1212820842712727552/XCuWn8yF_400x400.jpg");
+discord.SendMessageAsync(embeds: new Embed[] { testEmbed.Build() });
+```
+
+![Output](https://i.imgur.com/6O3Zs2D.png)
